9bc983e433da56e83fea9cc57b50acbd792d1d24
knakayama <knakayama.sh@gmail.com>
2015-04-01 19:52:40 +0900

    add mac support

--- scripts/avg.sh
+++ scripts/avg.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-uptime | perl -lne 's/\s+//g; printf "Avg:%s\n",$1 if /averages?:(.*)/'
+arch="$(uname -s)"
+
+if [[ "$arch" = "Linux" ]]; then
+    uptime | perl -lne 's/\s+//g; printf "Avg:%s\n",$1 if /averages?:(.*)/'
+elif [[ "$arch" = "Darwin" ]]; then
+    uptime | perl -lne 's/\s{2,}//g; s/:\s/:/; s/\s/,/g; printf "Avg:%s\n",$1 if /averages:\s?(.*)/'
+else
+    echo "Unkown platform"
+fi
 
